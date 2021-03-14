3b112b099d8b202493e057ecd4198cc0975506ca
sararighetti <71082145+sararighetti@users.noreply.github.com>
2021-03-14 11:46:19 +0100

    Update Exercici Queries SQL-Righetti Sara.sql

--- Exercici Queries SQL-Righetti Sara.sql
+++ Exercici Queries SQL-Righetti Sara.sql
@@ -1,38 +1,37 @@
-SELECT COUNT(*) FROM usairlineflights2.flights
+SELECT COUNT(*) FROM usairlineflights.flights;
 
 
 SELECT ORIGIN, AVG(ArrDelay) AS average_arrival, AVG(DepDelay) AS average_departure
- FROM usairlineflights2.flights 
- Group by Origin
+ FROM usairlineflights.flights 
+ Group by Origin;
  
  SELECT ORIGIN, COLYEAR, COLMONTH, AVG(ArrDelay) AS average_arrival
- FROM usairlineflights2.flights 
+ FROM usairlineflights.flights 
  Group by Origin, COLYEAR, COLMONTH
- ORDER BY Origin, colyear, colmonth
+ ORDER BY Origin, colyear, colmonth;
  
  
 SELECT CITY, COLYEAR, COLMONTH, AVG(ArrDelay) AS average_arrival
- FROM usairlineflights2.flights F
- JOIN usairlineflights2.usairports A
+ FROM usairlineflights.flights F
+ JOIN usairlineflights.usairports A
  ON F.ORIGIN = A.IATA
  Group by City, COLYEAR, COLMONTH
- ORDER BY City, colyear, colmonth
+ ORDER BY City, colyear, colmonth;
  
  SELECT UNIQUECARRIER, COLYEAR, COLMONTH, SUM(cancelled) AS total_cancelled
- FROM usairlineflights2.flights F
+ FROM usairlineflights.flights F
  Group by UNIQUECARRIER, COLYEAR, COLMONTH
- ORDER BY SUM(cancelled) DESC
+ ORDER BY SUM(cancelled) DESC;
  
  SELECT TAILNUM, SUM(DISTANCE) AS TotalDistance
- FROM usairlineflights2.flights F
+ FROM usairlineflights.flights F
  WHERE tailnum != ''
  GROUP BY TAILNUM
  ORDER BY sum(distance) DESC
- LIMIT 10
+ LIMIT 10;
  
 SELECT uniquecarrier, AVG(ArrDelay) AS average_arrival
- FROM usairlineflights2.flights 
+ FROM usairlineflights.flights 
  Group by uniquecarrier
  HAVING AVG(ArrDelay) >10
- ORDER BY AVG(ArrDelay) DESC
- 
+ ORDER BY AVG(ArrDelay) DESC;
